0de15b077e69af0db8fce9f247b74a1fff18f824
amar.lakshya <amar.lakshya@xaviers.edu.in>
2018-06-21 14:50:16 +0530

    UPDATED: DELETED: default.yml, ADDED: env vars

--- src/main.rs
+++ src/main.rs
@@ -1,36 +1,21 @@
 #[macro_use]
 extern crate clap;
 extern crate yaml_rust;
-use std::fs::File;
-use std::io::prelude::*;
 use std::env;
 
 use clap::App;
-use yaml_rust::YamlLoader;
-
 
 fn main()  ->   std::io::Result<()> {
+
     let yaml_file = load_yaml!("../cli.yml");
     let matches = App::from_yaml(yaml_file).get_matches();
-    let config = matches.value_of("CONFIG").unwrap_or("./default.yml");
 
     let package = matches.value_of("PACKAGE").unwrap();
     let ip = matches.value_of("IP").unwrap();
-    println!("values are: {}, {}, {}", config, package, ip);
 
-    //getting config file
-    let mut file = File::open(config)?;
-    let mut contents = String::new();
-    file.read_to_string(&mut contents)?;
-    let default_dir = format!("/home/{}/ros2_ws/", env::var("USER").unwrap());
+    let ros2_dir = env::var("ROS2_DIR").unwrap_or(format!("/home/{}/ros2_ws/", env::var("USER").unwrap()));
+    let package_dir = env::var("PACKAGE_DIR").unwrap_or(format!("/home/{}/ros2_ws/", env::var("USER").unwrap()));
 
-    let conf = YamlLoader::load_from_str(&contents).unwrap();
-    let conf = &conf[0];
-
-
-
-    let ros2_dir  =conf["ros2_dir"].as_str().unwrap_or(&default_dir);
-    let package_dir = conf["package_dir"].as_str().unwrap_or(&default_dir);
-    println!("values: {}, {}",  ros2_dir, package_dir);
+    println!("values are: {}, {}, {}, {}", ros2_dir, package_dir, package, ip);
     Ok(())
 }
